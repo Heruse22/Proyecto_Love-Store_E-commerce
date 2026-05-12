9a2e54628e0a08e3f4890043667b28a304401df8
Alejo <heruse22@gmail.com>
2026-05-12 13:57:46 -0500

    Conexión de la FakeStore API en api.js

--- js/api.js
+++ js/api.js
@@ -23,3 +23,27 @@ async function obtenerProductoPorId(id) {
     return null;
   }
 }
+
+// Trae todos los productos
+async function obtenerProductos() {
+  try {
+    const respuesta = await fetch(API_URL);
+    const productos = await respuesta.json();
+    return productos;
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    return [];
+  }
+}
+
+// Trae un solo producto por ID
+async function obtenerProductoPorId(id) {
+  try {
+    const respuesta = await fetch(`${API_URL}/${id}`);
+    const producto = await respuesta.json();
+    return producto;
+  } catch (error) {
+    console.error("Error al obtener producto:", error);
+    return null;
+  }
+}
