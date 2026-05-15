769e7e0288d99e73654c379891eaabafa6e035a2
Alejo <heruse22@gmail.com>
2026-05-15 15:52:04 -0500

    Implementacion de las logicas del carrito y las funciones de los productos: mostrar y traer al carrito

---
+++ js/main.js
@@ -0,0 +1,25 @@
+async function mostrarProductos() {
+    const productos = await obtenerProductos();
+    const contenedor = document.getElementById("productos");
+
+    productos.forEach(producto => {
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("tarjeta");
+        tarjeta.innerHTML = `
+            <img src="${producto.image}" alt="${producto.title}" />
+            <h3>${producto.title}</h3>
+            <p class="precio">$${producto.price}</p>
+            <button onclick="agregarAlCarrito(${producto.id})">Agregar al carrito</button>
+        `;
+        contenedor.appendChild(tarjeta);        
+    });
+    
+}
+
+function agregarAlCarrito(id) {
+    console.log("Producto agregado:", id);
+    // Aquí implementaré la lógica del carrito con la función de agregar al carrito
+    }
+
+    // Llamamos a la función para mostrar los prodcutos al crgar la página
+    mostrarProductos();
