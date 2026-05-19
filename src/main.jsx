823ebbd01de57a60549651335d4de8820599cd67
Heidy Ruse <heruse22@gmail.com>
2026-05-18 21:26:24 -0500

    Añadido archivo .env con variables de entorno de Firebase y reemplazado BrowserRouter por HashRouter en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode, useState } from 'react'  
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './styles/main.css'
 import App from './App.jsx'
 import Login from './pages/Login.jsx'
